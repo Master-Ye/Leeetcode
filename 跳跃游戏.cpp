70561b88e3844d864425f7356c697435db9c8714
masterder <1813708165@qq.com>
2023-08-25 20:07:32 +0800

    Update 跳跃游戏.cpp

--- 跳跃游戏.cpp
+++ 跳跃游戏.cpp
@@ -1,17 +1,12 @@
-class Solution
-{
+class Solution {
 public:
-    bool canJump(vector<int> &nums)
-    {
-        int cover = 0;
-        if (nums.size() == 1)
-            return true; // 只有⼀个元素，就是能达到
-        for (int i = 0; i <= cover; i++)
-        { // 注意这⾥是⼩于等于cover
-            cover = max(i + nums[i], cover);
-            if (cover >= nums.size() - 1)
-                return true;
-        }
-        return false;
-    }
+ bool canJump(vector<int>& nums) {
+ int cover = 0;
+ if (nums.size() == 1) return true; // 只有⼀个元素，就是能达到
+ for (int i = 0; i <= cover; i++) { // 注意这⾥是⼩于等于cover
+ cover = max(i + nums[i], cover);
+ if (cover >= nums.size() - 1) return true;
+ }
+ return false;
+ }
 };
